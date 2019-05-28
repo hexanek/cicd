681fc92ef7d7f5a50e8c05c951e4afcb77ea1bc0
hexanek <hexanek@gmail.com>
2019-05-28 13:11:56 +0200

    add clci config

--- src/main/java/pl/wedel/demo/DemoApplication.java
+++ src/main/java/pl/wedel/demo/DemoApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication
 {
-    
+
+    //dd
     public static void main(String[] args)
     {
         SpringApplication.run(DemoApplication.class, args);
